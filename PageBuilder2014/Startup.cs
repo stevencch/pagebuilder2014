d8218d1f98ba27b1a06bfd988f84516f104de6ba
stevenccoh <steven_caochao@hotmail.com>
2014-09-13 07:54:39 +1000

    init

---
+++ PageBuilder2014/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PageBuilder2014.Startup))]
+namespace PageBuilder2014
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
