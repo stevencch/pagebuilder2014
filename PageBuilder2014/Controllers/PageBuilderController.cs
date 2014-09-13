f1cf53ecc8ec7978ac31a1ca33edd5bc5d45f7b1
stevenccoh <steven_caochao@hotmail.com>
2014-09-13 10:26:12 +1000

    add templates

---
+++ PageBuilder2014/Controllers/PageBuilderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PageBuilder2014.Controllers
+{
+    public class PageBuilderController : Controller
+    {
+        // GET: PageBuilder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
