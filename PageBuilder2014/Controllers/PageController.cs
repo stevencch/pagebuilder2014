c4c55e747ea2a64ff50d142b2a4c3809005f7fea
stevenccoh <steven_caochao@hotmail.com>
2014-09-14 22:48:32 +1000

    new page

--- PageBuilder2014/Controllers/PageController.cs
+++ PageBuilder2014/Controllers/PageController.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Web;
 using System.Web.Http;
+using PageBuilder2014.Helper;
 using PageBuilder2014.Models;
 
 namespace PageBuilder2014.Controllers
 {
     public class PageController : ApiController
     {
+        private StringBuilder sbBegin = new StringBuilder();
         // GET: api/Page
         public IEnumerable<string> Get()
         {
@@ -25,6 +31,65 @@ namespace PageBuilder2014.Controllers
         // POST: api/Page
         public void Post(PageLayout pageLayout)
         {
+            string file1 =
+                HttpContext.Current.Server.MapPath("~/templates/t1/temppage.html");
+            string file2 =
+                HttpContext.Current.Server.MapPath("~/templates/t1/mypage.html");
+            GeneratePage(pageLayout,file1);
+            HtmlHelper.Process(file1,file2);
+
+        }
+
+        private void GeneratePage(PageLayout pageLayout,string file)
+        {
+            String page = File.ReadAllText(HttpContext.Current.Server.MapPath("~/content/templates/t1/html/home/p1.html"));
+            ProcessLayout(pageLayout);
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                string html = sbBegin.ToString() ;
+                page = page.Replace("[[PAGE]]", html);
+                sw.Write(page);
+            }
+        }
+
+        private void ProcessLayout(PageLayout pageLayout)
+        {
+            switch (pageLayout.pbnode)
+            {
+                case "root":
+                    sbBegin.Append("<div class='root'>");
+                    break;
+                case "c12":
+                    sbBegin.AppendLine("<div class='row'>");
+                    break;
+                case "c6a":
+                case "c6b":
+                    sbBegin.AppendLine("<div class='col-md-6'>");
+                    break;
+                case "c4a":
+                case "c4b":
+                case "c4c":
+                    sbBegin.AppendLine("<div class='col-md-4'>");
+                    break;
+                case "c8a":
+                case "c8b":
+                    sbBegin.AppendLine("<div class='col-md-8'>");
+                    break;
+                default:
+                    string html = File.ReadAllText(HttpContext.Current.Server.MapPath("~/content/templates/t1/html/home/"+pageLayout.pbnode+".html"));
+                    sbBegin.AppendLine("<!-- start "+pageLayout.pbnode+"-->");
+                    sbBegin.AppendLine(html);
+                    sbBegin.AppendLine("<!-- end " + pageLayout.pbnode + "-->");
+                    break;
+            }
+            foreach (var item in pageLayout.tree)
+            {
+                ProcessLayout(item);
+                if (!item.pbnode.StartsWith("s"))
+                {
+                    sbBegin.AppendLine("</div>");
+                }
+            }
         }
 
         // PUT: api/Page/5
---
+++ PageBuilder2014/Helper/HtmlHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace PageBuilder2014.Helper
+{
+    public class HtmlHelper
+    {
+        static StringBuilder newHtml = new StringBuilder();
+        static string html = "";
+        static int count = 0;
+        static bool isGroup = false;
+        static bool isLi = false;
+
+        public static void Process(String inputPath, String outputPath)
+        {
+            int i = 0;
+            string[] lines = File.ReadAllLines(inputPath);
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in lines)
+            {
+                sb.Append(line);
+            }
+            html = sb.ToString();
+            sb = new StringBuilder();
+            if (i == 0)
+            {
+                i = html.IndexOf("<body");
+            }
+            newHtml.Append(html.Substring(0, i));
+            SearchText(i);
+            File.WriteAllText(outputPath, newHtml.ToString());
+
+        }
+
+        private static void SearchText(int i)
+        {
+            int j = html.IndexOf('>', i);
+            string substring = html.Substring(i, j -i+ 1);
+            if (substring.StartsWith("<li"))
+            {
+                isLi = true;
+            }
+            if (substring.StartsWith("<br") || (isLi && substring.StartsWith("</li")))
+            {
+                isGroup = true;
+            }
+            if (isGroup && substring.StartsWith("</ul"))
+            {
+                isGroup = false;
+                isLi = false;
+            }
+            newHtml.Append(substring);
+            int k = html.IndexOf('<', j);
+            if (k >= 0)
+            {
+                if (k - j > 1)
+                {
+                    string text = html.Substring(j + 1, k-j-1).Trim();
+                    if (!substring.StartsWith("<script") && text.Length > 1)
+                    {
+                        ReplaceText(text);
+                    }
+                    else
+                    {
+                        newHtml.Append(text);
+                    }
+                }
+                SearchText(k);
+            }
+        }
+
+        private static void ReplaceText(String text)
+        {
+            if (!isGroup)
+            {
+                count++;
+            }
+            else
+            {
+                isGroup = false;
+            }
+
+            int first = 65 + (int)(count / 26);
+            int second = 65 + (count % 26);
+            string replaces = new StringBuilder().Append((char)first).Append((char)second).Append(' ').ToString();
+            int j = 0;
+            for (int i = 0; i < text.Length; i++, j++)
+            {
+                if (j > replaces.Length - 1)
+                {
+                    j = 0;
+                }
+                newHtml.Append(replaces[j]);
+            }
+            if (j == 1)
+            {
+                newHtml.Append(replaces[j]);
+            }
+        }
+    }
+}
